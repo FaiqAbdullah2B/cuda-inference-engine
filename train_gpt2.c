9ef91171e650071ea65efaca8e38cba9c5709c58
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2026-05-01 22:59:22 +0500

    Add Activation struct. Add QKV computation. Fix bug in init_parameters_sizes.

--- train_gpt2.c
+++ train_gpt2.c
@@ -3,6 +3,7 @@
 #include "./llmc/tokenizer.h"
 #include "./llmc/rand.h"
 #include <math.h>
+#include <string.h>
 
 typedef struct {
     int max_seq_len; // max sequence length
@@ -14,25 +15,40 @@ typedef struct {
     int num_parameters; // total trainable weights
 } GPT2Config;
 
-#define NUM_PARAMETER_TENSORS 4
+#define NUM_PARAMETER_TENSORS 6
 typedef struct {
     float *wte; // weight token embeddings
     float *wtp; // weight token positioning
     float *ln1w; // 1st layer normalization's weights
     float *ln1b;
+    float *qkvw;
+    float *qkvb;
 //    float *ln2w;
 //    float *ln2b;
 //    float *lnfw;
 //    float *lnfb;
 } ParameterTensors;
 
+#define NUM_ACTIVATION_TENSORS 3
+typedef struct {
+    float *encoded; // output of encoding
+    float *ln1;     // output of first layer normalization
+    float *qkv;     // output of qkv
+} ActivationTensors;
+
 typedef struct {
     GPT2Config config;
+    // parameters
     ParameterTensors params;
     size_t param_sizes[NUM_PARAMETER_TENSORS];
     float *params_memory;
     size_t num_parameters;
-    
+    // activations
+    ActivationTensors acts;
+    size_t act_sizes[NUM_ACTIVATION_TENSORS];
+    float *acts_memory;
+    size_t num_activations;
+
     int batch_size; // B
     int seq_len; // T
     int *inputs;
@@ -41,14 +57,16 @@ typedef struct {
 } GPT2;
 
 void init_parameters_sizes(size_t *param_sizes, GPT2Config config) {
-    size_t Vp = config.vocab_size;
+    size_t Vp = config.padded_vocab_size;
     size_t C = config.channels;
     size_t maxT = config.max_seq_len;
     size_t L = config.num_layers;
-    param_sizes[0] = Vp * C;       // wte
-    param_sizes[1] = maxT * C;     // wpe
-    param_sizes[2] = L * C;        // ln1w
-    param_sizes[3] = L * C;        // ln1b
+    param_sizes[0] = Vp * C;            // wte
+    param_sizes[1] = maxT * C;          // wpe
+    param_sizes[2] = L * C;             // ln1w
+    param_sizes[3] = L * C;             // ln1b
+    param_sizes[4] = L * (3 * C) * C;   // qkvw
+    param_sizes[5] = L * (3 * C);   // qkvb
 }
 
 // adds positional encoding and encoded token into an output
@@ -108,6 +126,28 @@ void layernorm_forward(float *out, float *inp, float *weight, float *bias,
     }
 }
 
+void matmul_forward(float *out,
+                    float *inp, float *weight, float *bias,
+                    int B, int T, int C, int OC) {    
+    // inp is (B,T,C), weight is (OC, C), bias is (OC)
+    // out will be (B,T,OC)
+    
+    for (int b = 0; b < B; b++) {
+        for (int t = 0; t < T; t++) {
+            float *out_bt = out + b * T * OC + t * OC;
+            float *inp_bt = inp + b * T * C + t * C;
+            for (int o = 0; o < OC; o++) {
+                float val = (bias != NULL) ? bias[o] : 0.0f;
+                float *wrow = weight + o*C;
+                for (int i = 0; i < C; i++) {
+                    val += inp_bt[i] * wrow[i];
+                }
+                out_bt[o] = val;
+            }
+        }
+    }
+}
+
 float *malloc_and_point_parameters(ParameterTensors *params, size_t *param_sizes) {
     size_t num_params = 0;
     for (size_t i = 0; i < NUM_PARAMETER_TENSORS; i++) {
@@ -119,7 +159,8 @@ float *malloc_and_point_parameters(ParameterTensors *params, size_t *param_sizes
 
     // assign all the tensors
     float **ptrs[] = {
-        &params->wte, &params->wtp, &params->ln1w, &params->ln1b
+        &params->wte, &params->wtp, &params->ln1w, &params->ln1b, 
+        &params->qkvw, &params->qkvb
     };
     
     // set all the starting locations for pointers
@@ -129,6 +170,25 @@ float *malloc_and_point_parameters(ParameterTensors *params, size_t *param_sizes
         params_memory_iterator += param_sizes[i];
     }
     return params_memory;
+}
+
+float *malloc_and_point_activations(ActivationTensors *acts, size_t *act_sizes) {
+    size_t num_activations = 0;
+    for (size_t i = 0; i < NUM_ACTIVATION_TENSORS; i++) {
+        num_activations += act_sizes[i];
+    }
+    
+    float *acts_memory = (float*)malloc(num_activations * sizeof(float));
+    float **ptrs[] = {
+        &acts->encoded, &acts->ln1, &acts->qkv
+    };
+
+    float *acts_memory_iterator = acts_memory;
+    for (size_t i = 0; i < NUM_ACTIVATION_TENSORS; i++) {
+        *(ptrs[i]) = acts_memory_iterator;
+        acts_memory_iterator += act_sizes[i];
+    }
+    return acts_memory;
 }
 
 void gpt2_build_from_checkpoint(GPT2 *model, const char *checkpoint_path) {
@@ -177,6 +237,7 @@ void gpt2_build_from_checkpoint(GPT2 *model, const char *checkpoint_path) {
     fclose(model_file);
 
     // other inits
+    model->acts_memory = NULL;
     model->inputs = NULL;
     model->targets = NULL;
     model->seq_len = 0;
@@ -204,25 +265,72 @@ void gpt2_forward(GPT2 *model, int *inputs, int *targets, size_t B, size_t T) {
         }
     }
 
+    if (model->acts_memory == NULL) {
+        model->batch_size = B;
+        model->seq_len = T;
+
+        // allocate space
+        model->act_sizes[0] = B * T * C;            // encoded
+        model->act_sizes[1] = L * B * T * C;        // ln1
+        model->act_sizes[2] = L * B * T * C * 3;    // qkv
+        size_t num_activations = 0;
+        for (int i = 0; i < NUM_ACTIVATION_TENSORS; i++) {
+            num_activations += model->act_sizes[i];
+        }
+        printf("num_activations: %ld\n", num_activations);
+        model->num_activations = num_activations;
+        model->acts_memory = malloc_and_point_activations(&model->acts, model->act_sizes);
+        model->inputs = (int*)malloc(B * T * sizeof(int));
+        model->targets = (int*)malloc(B * T * sizeof(int));
+    }
+    else {
+        if (B != model->batch_size || T != model->seq_len) {
+            printf("Model: B=%d T=%d, Desired: B=%ld T=%ld\n", 
+                   model->batch_size, model->seq_len, B, T);
+        }
+    }
+
+    // cache the inputs/targets
+    memcpy(model->inputs, inputs, B * T * sizeof(int));
+    if (targets != NULL) {
+        memcpy(model->targets, targets, B * T * sizeof(int));
+    }
+
     ParameterTensors params = model->params;
-    float *output_encoded = (float*)malloc(sizeof(float) * B * T * C);
-    encoder_forward(output_encoded, inputs, params.wte, params.wtp, B, T, C);
-    
+    ActivationTensors acts = model->acts;
+    float* residual;
+
+    encoder_forward(acts.encoded, inputs, params.wte, params.wtp, B, T, C);
     printf("=================== encoder output ==================\n");
     for (int i = 0; i < B * T; i++) {
-        printf("%f ", output_encoded[i]);
+        printf("%f ", acts.encoded[i]);
     }
-    printf("\n"); 
+    printf("\n");
+
     float *ln1w = params.ln1w;
     float *ln1b = params.ln1b;
-    float *ln1 = (float*)malloc(sizeof(float) * B * T * C);
-    layernorm_forward(ln1, output_encoded, ln1w, ln1b, B, T, C);
-
+    layernorm_forward(acts.ln1, acts.encoded, ln1w, ln1b, B, T, C);
     printf("=================== layernorm output ==================\n");
     for (int i = 0; i < B * T; i++) {
-        printf("%f ", ln1[i]);
+        printf("%f ", acts.ln1[i]);
+    }
+    printf("\n");
+
+    for (int l = 0; l < L; l++) {
+        float *ln1_l = acts.ln1 + l * B * T * C;
+
+        float *qkv_l = acts.qkv + l * B * T * (3 * C);
+
+        float *qkvw_l = params.qkvw + l * (3 * C * C);
+        float *qkvb_l = params.qkvb + l * (3 * C);
+
+        matmul_forward(qkv_l, ln1_l, qkvw_l, qkvb_l, B, T, C, 3 * C);
     }
 
+    printf("=================== QKV output ==================\n");
+    for (int i = 0; i < B * T; i++) {
+        printf("%f ", acts.qkv[i]);
+    }
     printf("\n");
 }
 
